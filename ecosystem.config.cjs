0a520b6f865895fa87dad156885ffce10f261b2e
machangzhe <machangzhe@gmail.com>
2025-09-21 23:21:29 +0800

    监控订单簿变化

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -19,6 +19,16 @@ module.exports = {
             env: {
                 NODE_ENV: 'production'       // 设置环境变量为生产环境
             }
+        },
+        {
+            name: 'MMTrade',
+            script: './dist/trade/market_maker/index.js',     // pm2 启动的入口文件
+            watch: false,                  // 是否监控文件变动重启，可根据需求开启
+            instances: 1,                  // 启动的实例数量
+            exec_mode: 'fork',             // fork 模式
+            env: {
+                NODE_ENV: 'production'       // 设置环境变量为生产环境
+            }
         }
     ]
 };
---
+++ src/trade/market_maker/index.ts
@@ -0,0 +1,27 @@
+import { Spot, SPOT_WS_STREAMS_PROD_URL } from '@binance/spot';
+import { SYMBOL } from '../../config.js';
+
+const configurationWebsocketStreams = {
+    wsURL: process.env.WS_STREAMS_URL ?? SPOT_WS_STREAMS_PROD_URL,
+};
+const client = new Spot({ configurationWebsocketStreams });
+
+const diffBookDepth = async () => {
+    let connection;
+
+    try {
+        connection = await client.websocketStreams.connect();
+
+        const stream = connection.diffBookDepth({
+            symbol: SYMBOL,
+        });
+
+        stream.on('message', (data) => {
+            console.info(data);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+diffBookDepth();
