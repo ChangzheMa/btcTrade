9f93a5a4cb59d412a8a5505ce4fd93768ce39111
machangzhe <machangzhe@gmail.com>
2025-09-28 00:51:03 +0800

    略微修改交易参数

--- src/trade/market_maker/params.ts
+++ src/trade/market_maker/params.ts
@@ -7,7 +7,7 @@ export const PRICE_GAP_LOWER_LIMIT: {[k: string]: number} = {
 
 // 未完成订单等待多久后抛弃
 export const UN_FILL_ORDER_WAIT_SECOND: {[k: string]: number} = {
-    [SYMBOL]: 1
+    [SYMBOL]: 2.5
 }
 
 // 单次下单数量
