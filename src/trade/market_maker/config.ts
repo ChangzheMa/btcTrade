852ab56e18269e505a3359a8a57fefdfd3e9ba1c
machangzhe <machangzhe@gmail.com>
2025-09-29 02:15:47 +0800

    fix bugs

--- src/trade/market_maker/config.ts
+++ src/trade/market_maker/config.ts
@@ -1,6 +1,6 @@
 export const SYMBOL = process.env.SYMBOL || ''
 
-export const QUANTITY_PRECISION_MAP = {
+export const QUANTITY_PRECISION_MAP: {[k: string]: number}  = {
     'BTCFDUSD': 5,
     'BNBFDUSD': 3
 }
