b6e18bc70cd1c970ba2a508efbf1dad022660f68
Changzhe Ma <machangzhe@gmail.com>
2025-11-07 15:34:04 +0800

    更新市场监控脚本的启动命令

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -48,9 +48,23 @@ module.exports = {
         //         API_SECRET: 'aamMkjGbmtTqqV1uXbJKmmEIQpIGyjMasQzkbskoeSKKsPJMTPWqbv5gWjligVM5',
         //     }
         // },
+        // {
+        //     name: 'GridMakerBTC',
+        //     script: './dist/trade/grid_maker/index.js',     // pm2 启动的入口文件
+        //     watch: false,                  // 是否监控文件变动重启，可根据需求开启
+        //     instances: 1,                  // 启动的实例数量
+        //     exec_mode: 'fork',             // fork 模式
+        //     env: {
+        //         NODE_ENV: 'production',       // 设置环境变量为生产环境
+        //         SYMBOL: 'BTCFDUSD',
+        //         BASE_COIN: 'FDUSD',
+        //         API_KEY: '5vrSGNpTJAJkNYyDHTlb7uLXdMXCJRoNATRdyxJwhLe0Xn9G89n0m0aw6GL7nroa',
+        //         API_SECRET: 'uP6guqd7U8m5e5BdJte64xM86c4xru8EL56yTU0NnTqT3PoVbw18h4HFdRj7acji',
+        //     }
+        // },
         {
-            name: 'GridMakerBTC',
-            script: './dist/trade/grid_maker/index.js',     // pm2 启动的入口文件
+            name: 'MarketWatchAll',
+            script: './dist/trade/market_watch/index.js',     // pm2 启动的入口文件
             watch: false,                  // 是否监控文件变动重启，可根据需求开启
             instances: 1,                  // 启动的实例数量
             exec_mode: 'fork',             // fork 模式
