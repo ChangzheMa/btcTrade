25829c5a998854e00424eb06242a859589050571
machangzhe <machangzhe@gmail.com>
2025-11-11 01:34:08 +0800

    remove unused log

--- src/trade/market_watch/cache.ts
+++ src/trade/market_watch/cache.ts
@@ -18,7 +18,7 @@ class MarketWatchCache {
      * @param trade - 从 WebSocket 收到的成交事件
      */
     public addTrade(trade: TradeEvent): void {
-        console.log(`addTrade: ${JSON.stringify(trade)}`)
+        // console.log(`addTrade: ${JSON.stringify(trade)}`)
 
         const symbol = trade.s;
         if (!this.tradeCache.has(symbol)) {
